d8dbda88d24c9c5b809d155d4738b661e89c41f4
kaushal <kolhekaushal2003@gmail.com>
2024-08-06 22:14:34 +0530

    Acess Modifier added

---
+++ Core_Java_Oops/Access_Modifier.java
@@ -0,0 +1,11 @@
+package Java_oops;
+
+
+public class Access_Modifier {
+public static void main(String[] args) {
+	
+	Access_Modifier1 ac= new Access_Modifier1();
+	System.out.println(ac.marks);
+	ac.send();
+}
+}
---
+++ Core_Java_Oops/Access_Modifier1.java
@@ -0,0 +1,12 @@
+package Java_oops;
+
+public class Access_Modifier1 {
+	  int marks=23;
+	public void send() {
+		System.out.println("hello");
+	}
+}
+//default
+//private 
+//protected
+//public
